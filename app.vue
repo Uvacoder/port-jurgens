ab351bd7220630d104a3971daab8a2c23b7eeeb3
d-jurgens <jurgens.david@gmail.com>
2022-10-01 20:57:17 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-xl text-on-background dark:text-on-dark-background">
+            {{ colorMode.preference }}
+        </h1>
+        <select v-model="colorMode.preference">
+            <option value="dark">Dark</option>
+            <option value="light">Light</option>
+            <option value="system">System</option>
+        </select>
+    </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: [],
+    theme: {
+        colors: {
+            background: "#fcfcff",
+            "dark-background": "#1a1c1e",
+            "on-background": "#1a1c1e",
+            "on-dark-background": "#e2e2e5",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
