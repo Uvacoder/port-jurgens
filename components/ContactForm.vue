763fad792a22fd692c3ded9392a44f02790bd354
d-jurgens <jurgens.david@gmail.com>
2022-10-08 17:50:53 +0200

    feat: form functionality

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -3,27 +3,60 @@
 		class="p-6 mx-auto bg-white dark:bg-slate-800 max-w-xl rounded-lg shadow-lg"
 	>
 		<FormKit type="form" @submit="sendForm" submit-label="Send">
-			<FormKit type="text" name="name" label="Name" />
+			<FormKit type="text" name="name" label="Name" v-model="name" />
 			<FormKit
 				type="email"
 				name="email"
 				label="Email"
 				validation="required|email"
+				v-model="email"
 			/>
 			<FormKit
 				type="textarea"
 				name="message"
 				label="Message"
 				validation="required"
+				v-model="message"
 			/>
 		</FormKit>
 	</div>
 </template>
 
 <script setup lang="ts">
+const email = ref('');
+const name = ref('');
+const message = ref('');
 const sending = ref(false);
 
-const sendForm = () => {
+const sendForm = async () => {
 	sending.value = true;
+	await $fetch('https://email.jurgens-david8261.workers.dev/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			personalizations: [
+				{
+					to: [
+						{
+							email: 'no-reply@davidjurgens.com',
+						},
+					],
+				},
+			],
+			from: {
+				email: 'no-reply@davidjurgens.com',
+			},
+			subject: 'Form submission from davidjurgens.com',
+			content: [
+				{
+					type: 'text/plain',
+					value: `New message from: ${name.value}, ${email.value}: ${message.value}`,
+				},
+			],
+		}),
+	});
+	sending.value = false;
 };
 </script>
--- formkit.config.js
+++ formkit.config.js
@@ -1,8 +1,10 @@
 import { generateClasses } from '@formkit/themes';
 import formkitTheme from './formkitTheme.js';
+import { createAutoAnimatePlugin } from '@formkit/addons';
 
 export default {
 	config: {
 		classes: generateClasses(formkitTheme),
 	},
+	plugins: [createAutoAnimatePlugin()],
 };
